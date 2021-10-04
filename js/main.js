dfec12b71628bdfdd0b271d85b5b2a119feef445
aanchal <fatwaniaanchal@gmail.com>
2021-10-05 01:58:38 +0530

    fix(perf): Compressed imgs, redundant html, css and js removed

--- js/main.js
+++ js/main.js
@@ -35,31 +35,6 @@
 	// Scrollax
    $.Scrollax();
 
-	var carousel = function() {
-		$('.carousel-testimony').owlCarousel({
-			center: false,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 4
-				}
-			}
-		});
-
-	};
-	carousel();
-
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
 		// 	 timer;
